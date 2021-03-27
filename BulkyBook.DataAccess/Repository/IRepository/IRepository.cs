bf66eafb5169d3cdcff28097b4dd7ff5817cf2d3
hachemchrayti <hachemfst@gmail.Com>
2021-03-27 13:55:45 +0100

    Create Repositry base class and Repository base interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+        );
+
+
+        T GetFirstOrdefault(
+          Expression<Func<T, bool>> filter = null,
+          string includeProperties = null
+      );
+
+
+
+        void Add(T entity);
+
+
+        void Remove(int id);
+
+
+        void Remove(T entity);
+
+
+        void RemoevRange(IEnumerable<T> entity);
+
+    }
+}
