d6aaf1f4aefe4a606c745a952410b14679595d2a
hachemchrayti <hachemfst@gmail.Com>
2021-03-27 17:47:19 +0100

    IUnitOfWork example => Convert CoverType CRUD from using repository to another repository(stored proc)

--- BoolKyBookMVCNet5/Areas/Admin/Controllers/CoverTypeController.cs
+++ BoolKyBookMVCNet5/Areas/Admin/Controllers/CoverTypeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -37,8 +39,9 @@ namespace BoolKyBookMVCNet5.Areas.Admin.Controllers
             {
                 return View(coverType);
             }
-
-            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter); //_unitOfWork.CoverType.Get(id.GetValueOrDefault());
             if (coverType == null)
             {
                 return NotFound();
@@ -54,13 +57,17 @@ namespace BoolKyBookMVCNet5.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", coverType.Name);
                 if (coverType.Id == 0)
                 {
-                    _unitOfWork.CoverType.Add(coverType);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter); //_unitOfWork.CoverType.Add(coverType);
                 }
                 else
                 {
-                    _unitOfWork.CoverType.Update(coverType);
+                    parameter.Add("@Id", coverType.Id);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
+                    // _unitOfWork.CoverType.Update(coverType);
                 }
 
                 _unitOfWork.Save();
@@ -76,21 +83,22 @@ namespace BoolKyBookMVCNet5.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.CoverType.GetAll();
+            var allObj = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll, null); //_unitOfWork.CoverType.GetAll();
             return Json(new { data = allObj });
         }
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-
-            var objFromDB = _unitOfWork.CoverType.Get(id);
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            var objFromDB = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter); //_unitOfWork.CoverType.Get(id);
             if (objFromDB == null)
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
 
-            _unitOfWork.CoverType.Remove(objFromDB);
+            _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameter);//_unitOfWork.CoverType.Remove(objFromDB);
 
             _unitOfWork.Save();
 
